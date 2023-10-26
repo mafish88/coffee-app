2527adccd519312564a536379081a9be0c541d94
Michael Fisher <mafish88@icloud.com>
2023-10-26 13:48:13 -0400

    react_coffee_app

---
+++ src/components/CoffeeList.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+export default function CoffeeList() {
+    const [coffees, setCoffees] = useState()
+    const getCoffees = (temperature) => {
+        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+        .then(res => res.json())
+        .then(data => setCoffees(data))
+        .catch(alert)
+    }
+
+    return (
+        <main>
+            <div>
+                <button onClick={()=> getCoffees('hot')}>Hot</button>
+                <button onClick={()=> getCoffees('iced')}>Iced</button>
+
+            </div>
+            <section>
+                {coffees &&
+                    coffees.map(coffee => (
+                        <div key={coffee.id}>
+                            <img src={coffee.image} alt={coffee.title} />
+                            <h2>{coffee.title}</h2>
+                            <p>{coffee.description}</p>
+                        </div>
+                    ))
+                }
+            </section>
+
+
+        </main>
+
+
+
+
+
+
+
+    )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>
+            {/* %copy; {new_Date().getFullYear()} Michael Fisher */}
+                footer
+            </p>
+        </footer>
+    )
+}
